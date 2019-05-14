83cdc213307a75b769225f11de328ea33412c24c
Mikelamontgomery <mikelamontgomery32@gmail.com>
2019-05-13 21:22:48 -0500

    Java

---
+++ index.js
@@ -0,0 +1,28 @@
+  alert('Hello world!');
+
+
+var MyArray =(1,2, "3")
+
+
+var myFaves= {
+    pet: "cat",
+    drink: "chocholate milk",
+    number: 1,
+    array: [1,2,3],
+    list: myFaves,  
+    };
+
+    const name = prompt("Shay");
+
+const checkName = function(prompt) {
+    var name = prompt("What is your name?");
+  if (name === "") {
+    name = prompt("What is your name, for real this time?");
+    checkName(); // repeats until a name exists
+  } 
+alert("hello, ")
+};
+
+checkName(); // kicks off the name-checking the first time
+
+alert("Hello " + Mikela);
